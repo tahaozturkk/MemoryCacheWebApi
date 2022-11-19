bb1cd92d8c3b77778f5a791e6508d44b21b6b822
tahaozturkk <ozturktaha1453@gmail.com>
2022-11-19 17:55:37 +0300

    Add project files.

---
+++ MemoryCacheWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace MemoryCacheWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<string> Values = new();
+            Values = _memoryCache.Get<List<string>>("Values");
+
+            if (Values is null)
+            {
+                Values = new()
+            {
+                "MemoryCacheTest1",
+                "MemoryCacheTest2",
+                "MemoryCacheTest3",
+                "MemoryCacheTest4",
+                "MemoryCacheTest5",
+                "MemoryCacheTest6",
+            };
+                Thread.Sleep(3000);
+                _memoryCache.Set("Values", Values, TimeSpan.FromSeconds(10));
+            }
+
+            return Ok(Values);
+        }
+    }
+}
